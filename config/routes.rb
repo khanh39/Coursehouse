714f0312918cddbd2f043f7d4a56eae23d8fe8a1
Hamza Khan <hamzakhan@Fatimas-MacBook-Pro-2.local>
2016-10-03 16:17:04 -0400

    Created log in function

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get 'users/new'
 
   get 'users/create'
 
   get 'courses' => 'courses#index'
   get 'courses/:id' => 'courses#show'
+
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :users, only: [:new, :create]
 end
