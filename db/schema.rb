04b21b99c2a230a35df71c40e1ca537a4713b60f
Hamza Khan <khanh39@mcmaster.ca>
2016-10-04 19:44:51 -0400

    migration for user name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003233523) do
+ActiveRecord::Schema.define(version: 20161004204639) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "file"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20161003233523) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
 end
