f645505d4bfd38cce6e6d9430f5d851fce85f178
Hamza Khan <khanh39@mcmaster.ca>
2016-10-04 14:34:04 -0400

    added course validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,8 @@
 class Course < ApplicationRecord
     has_many :users, :through => :payments
     has_many :payments
+    validates :description, :title, presence: true
+    validates :price_in_cents, numericality: {only_integer: true}
 end
 
 #next step: seeding, make some fake courses
