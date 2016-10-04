1188bc2013e6fb4e9d7d8a7aa7dd0cd79e37bbfc
Hamza Khan <khanh39@mcmaster.ca>
2016-10-04 19:40:52 -0400

    moved some methods from app record to app controller

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  protect_from_forgery with: :exception
-
-  private
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  helper_method :current_user
+  self.abstract_class = true
 end
