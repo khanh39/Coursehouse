2dc34da2d7c0199432160943a1e1d9f48463dfe0
Hamza Khan <khanh39@mcmaster.ca>
2016-10-04 19:41:15 -0400

    created payments controller

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,19 @@
+class PaymentsController < ApplicationController
+  before_action :ensure_logged_in, only: [:create, :destroy]
+
+  def create
+    @payment = Payment.new(payment_params)
+    @payment.user_id = current_user.id
+    if @payment.save
+      redirect_to courses_url, notice: "Payment made successfully!"
+    else
+      render 'courses/show'
+    end
+  end
+
+  private
+  def payment_params
+    #course id
+  end
+
+end
