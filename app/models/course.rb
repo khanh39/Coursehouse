7756fc70363371da652ddcc6b22902df10c352d3
Hamza Khan <khanh39@mcmaster.ca>
2016-10-04 15:24:08 -0400

    Diplaying price in dollars

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,11 @@ class Course < ApplicationRecord
     has_many :payments
     validates :description, :title, presence: true
     validates :price_in_cents, numericality: {only_integer: true}
+
+    def formatted_price
+      price_in_dollars = price_in_cents.to_f / 100
+      sprintf("%.2f", price_in_dollars)
+    end
 end
 
 #next step: seeding, make some fake courses
